e9e8ad84c3899a11cb3bb46ea014d14256e83db1
dev-dmitriy-diachenko <dev.dmitriy.diachenko@gmail.com>
2022-11-12 15:58:04 +0200

    added sign-in and sign-up pages

---
+++ src/pages/SignIn/SignIn.page.jsx
@@ -0,0 +1,9 @@
+import { SignInForm } from '../../components/Forms';
+
+import './SignIn.page.scss';
+
+export const SignIn = () => (
+  <div className="sign-in-page">
+    <SignInForm />
+  </div>
+);
---
+++ src/pages/SignUp/SignUp.page.jsx
@@ -0,0 +1,9 @@
+import { SignUpForm } from '../../components/Forms';
+
+import './SignUp.page.scss';
+
+export const SignUp = () => (
+  <div className="sign-up-page">
+    <SignUpForm />
+  </div>
+);
